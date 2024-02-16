d9cc2b3580682db706087d568d5ddbb0ab280847
snowkluster <rohankaushik1@outlook.com>
2024-02-16 22:00:34 +0530

    updated clock

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ function showTime() {
 		weekday: "long",
 		hour: "2-digit",
 		minute: "2-digit",
+		hour12: true,
 		second: "2-digit",
 		day: "2-digit",
 		month: "2-digit",
@@ -15,8 +16,12 @@ function showTime() {
 
 	const formattedDate = date.toLocaleString("en-GB", options);
 	const [dayOfWeek, dateStr, time] = formattedDate.split(", ");
-
-	dateElement.innerHTML = `<h2>${dayOfWeek}, ${time} | ${dateStr}</h2>`;
+	const mq = window.matchMedia( "(min-width: 338px)" );
+	if (mq.matches){
+		dateElement.innerHTML = `<h2>${dayOfWeek}, ${time} | ${dateStr}</h2>`;
+	} else {
+		dateElement.innerHTML = `<h2>${dayOfWeek}, ${time} ${dateStr}</h2>`;
+	}
 }
 
 setInterval(showTime, 1000);
